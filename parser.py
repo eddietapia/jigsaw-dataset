a2bb06274e0c0be34f3e6c2f07ce3f89f13c14f1
etapiahe <etapiahe@ucsd.edu>
2019-04-20 17:05:40 -0400

    added test numpy data

--- parser.py
+++ parser.py
@@ -1,9 +1,20 @@
 import numpy as np
-import os 
+import warnings
+import os
+
+import pandas as pd
+
 # Read in the filenames and 
 with open('./Experimental_setup/Knot_Tying/Balanced/GestureClassification/OneTrialOut/10_Out/itr_1/Train.txt') as f:
      file_names = f.readlines()
 file_data = np.loadtxt('./Knot_Tying/kinematics/AllGestures/Knot_Tying_B001.txt')
+
+# 0 -> novice, 1 -> intermediate, 2 -> expert
+class_values = {'B': 0, 'C': 1, 'D': 2, 'E': 2, 'F': 1, 'G': 0, 'H': 0, 'I': 0}
+DATA_INDEX = 0
+LABEL_TYPE_INDEX = 1
+SURGEON_INDEX = 2
+CLASS_INDEX = 3
 
 class PreProcess(): 
     def __init__(self, directory):
@@ -43,12 +54,16 @@ class PreProcess():
             3). Skill detection
         '''  
         leave_out="OneTrialOut"
-        gClass = self.get_from_labelType(self.directory, data_type, "GestureClassification", leave_out)
-        gRec = self.get_from_labelType(self.directory, data_type, "GestureRecognition", leave_out)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            gClass = self.get_from_labelType(self.directory, data_type, "GestureClassification", leave_out)
+            gRec = self.get_from_labelType(self.directory, data_type, "GestureRecognition", leave_out)
 
         if (data_type == "Suturing"):
-            skillDet = self.get_from_labelType(self.directory, data_type, "SkillDetection", leave_out)
-            return gClass, gRec, skillDet
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                skillDet = self.get_from_labelType(self.directory, data_type, "SkillDetection", leave_out)
+                return gClass, gRec, skillDet
         return gClass, gRec
     
     
@@ -80,17 +95,25 @@ class PreProcess():
         output_data = []
         print("Printing data, ")
         print(data)
-        for i in range(10):
+        for i in range(len(data)):
             row_data = []
             txt_file = data[i][0].split('_')
+            label = data[i][1]
+            # Find out which surgeon it is
+            if data_type == 'Suturing':
+                surgeon = txt_file[1][0]
+            else:
+                surgeon = txt_file[2][0]
+            class_value = class_values[surgeon]
             file_name = self.get_file_name(txt_file, data_type)
             row_start, row_end = self.get_start_and_end(txt_file, data_type)
+            # Read in the appropriate files for the kinematics
             with open('./' + data_type + '/kinematics/AllGestures/' + file_name) as f:
                 rows = f.readlines()
             for j in range(row_start, row_end):
                 current_row = rows[j].split()
                 row_data.append(current_row)
-            output_data.append(row_data)
+            output_data.append([row_data, label, surgeon, class_value])
         return output_data
 
     def parse(self):
@@ -108,10 +131,10 @@ class PreProcess():
         
         # Iterate through the knot_tying data
         kt_gest_class_data = self.read_data(knot_tying_data[0], 'Knot_Tying')
-        kt_gest_rec_data = self.read_data(knot_tying_data[1], 'Knot_Tying')
-        table['knot_tying'] = {'gesture_classification': kt_gest_class_data, 
-                               'gesture_recognition': kt_gest_rec_data}
-
+        #kt_gest_rec_data = self.read_data(knot_tying_data[1], 'Knot_Tying')
+        table['knot_tying'] = {'gesture_classification': kt_gest_class_data} #, 
+                               #'gesture_recognition': kt_gest_rec_data}
+        '''
         # Iterate through the suturing data
         sut_gest_class_data = self.read_data(suturing_data[0], 'Suturing')
         sut_gest_rec_data = self.read_data(suturing_data[1], 'Suturing')
@@ -124,6 +147,38 @@ class PreProcess():
         np_gest_class_data = self.read_data(needle_passing[0], 'Needle_Passing')
         np_gest_rec_data = self.read_data(needle_passing[1], 'Needle_Passing')
         table['needle_passing'] = {'gesture_classification': np_gest_class_data, 
-                               'gesture_recognition': np_gest_rec_data}
+                               'gesture_recognition': np_gest_rec_data}'''
         
         return table
+
+p = PreProcess( "./")
+d = p.parse()
+
+csv_columns = ['Task','Label_Type','Data', 'Gesture/Skill', 'Surgeon', 'Class_Value']
+
+df = pd.DataFrame(columns=csv_columns)
+data_ = { 
+            'Task': [],
+            'Label_Type': [],
+            'Data': [], 
+            'Gesture/Skill': [], 
+            'Surgeon': [], 
+            'Class_Value': []
+        }
+try:
+    for task in d.keys():
+        for label_type in d[task].keys():
+            for row in d[task][label_type]:
+                data_['Task'].append(task)
+                data_['Label_Type'].append(label_type)
+                data_['Data'].append(row[0])
+                data_['Gesture/Skill'].append(row[1])
+                data_['Surgeon'].append(row[2])
+                data_['Class_Value'].append(row[3])
+
+except IOError:
+    print("I/O error") 
+
+print("Saving full dataset into numpy object")
+np.save('data_full.npy', data_)
+print("Finished saving full dataset")
